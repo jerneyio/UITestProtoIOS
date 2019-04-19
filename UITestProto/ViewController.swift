d4bf1453f6c00cc65c9e076b03f28c61f530369c
Josh Erney <Jerney@ms3-inc.com>
2019-04-19 11:50:45 -0600

    Initial Commit

---
+++ UITestProto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestProto
+//
+//  Created by Joshua Erney on 4/19/19.
+//  Copyright © 2019 Army. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
